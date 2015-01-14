4ad28c62e9b3c7d99df692a99ee4e6bec57b0f5a
Matvey-Kuk <motakuk@gmail.com>
2015-01-14 22:03:39 +0300

    fixtures

--- start.sh
+++ start.sh
@@ -2,5 +2,11 @@
 cd "/opt/app"
 
 pip3 install -r requirements.txt;
+
 python3 migrate
+
+if [ ! -f db.sqlite3 ]; then
+    python manage.py loaddata  fixtures;
+fi
+
 python3 manage.py runserver 0.0.0.0:80
