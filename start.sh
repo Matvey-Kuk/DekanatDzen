4e42a89f3e742d0fe57dc0bab2b31ef263fe2243
Matvey-Kuk <motakuk@gmail.com>
2015-01-14 22:25:47 +0300

    soc btns

--- start.sh
+++ start.sh
@@ -3,10 +3,6 @@ cd "/opt/app"
 
 pip3 install -r requirements.txt;
 
-python3 migrate
-
-if [ ! -f db.sqlite3 ]; then
-    python manage.py loaddata  fixtures;
-fi
+python3 manage.py migrate
 
 python3 manage.py runserver 0.0.0.0:80
